17b11f273564f47b6ca5f71660882c66060fe086
Alejandro <gar231136@gmail.com>
2024-10-09 23:01:01 -0600

    Actualización final

--- Grupo_10_Test_Primalidad.py
+++ Grupo_10_Test_Primalidad.py
@@ -30,25 +30,25 @@ def Criba(n):
             not_primes.add(j)
         # Después de las validaciones añade a i al array de primos
         primes.append(i)
-    print(primes)
     return primes
 
 def prime_test2(n):
     # Llama a funcion Criba y guarda los número primos en otro arreglo
     prime_list = Criba(int(n**0.5))
-    print(prime_list)
     # Verifica divisibilidad del n según la lista de primos
     for i in prime_list:
         # Si el número es divisible dentro de algún primo, entonces, es compuesto
         if n % i==0:
+            print("El número ", n, " no es primo, pues lo divide ", i)
             return False
+    print("El número ", n, " es primo")
     return True
 
 
 print(" ------------------------------------------------")
 print(" |              TEST DE PRIMALIDAD              |")
 print(" ------------------------------------------------")
-num = validacion(input("\n - Introduce un número: "))
-print(prime_test2(num))
+num = validacion(input(" - Introduce un número: "))
+prime_test2(num)
 
 
